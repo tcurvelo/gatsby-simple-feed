1d240636b8fff455990ba10a5615ea2f2251ff37
Thiago Curvelo <tcurvelo@gmail.com>
2021-01-23 20:52:53 -0300

    Add a simple index page to list the items

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,32 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+export default function IndexPage({data}) {
+  const items = data.items.nodes;
+  return (
+    <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+              <a href={item.url}>
+                <h2>{item.title}</h2>
+                <img src={item.image} alt={item.title} />
+              </a>
+          </li>
+        ))}
+    </ul>
+  )
+}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+export const query = graphql`
+  query {
+    items: allItem(filter: {url: {ne: null}}) {
+      nodes {
+        price
+        title
+        url
+        image
+        id
+      }
+    }
+  }
+`
